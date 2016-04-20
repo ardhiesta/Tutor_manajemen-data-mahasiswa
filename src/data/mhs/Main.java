9598d90c70f513a99ed5b0e5fd9312c2e2170d22
ardhiesta <ardhi.mail@gmail.com>
2016-04-21 05:58:43 +0700

    tambah data project

---
+++ src/data/mhs/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.mhs;
+
+/**
+ *
+ * @author linuxluv
+ */
+public class Main {
+    //method utama, yang pertama kali dieksekusi
+    public static void main(String[] args) {
+        
+    }
+}
